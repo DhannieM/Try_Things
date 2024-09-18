de62c80cb5171154ba770f2d9cecc9ac7a909f67
DhannieM <dg7489@rit.edu>
2024-09-18 13:49:11 -0400

    Starting pokedex

---
+++ Practice_program_Pokedex/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice_program_Pokedex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====Welcome to Console.PKDX=====\n" +
+                "Enter your inquiry:\n" +
+                "\n" + 
+                "Pokemon Search\n" +
+                "Type\n" +
+                "Moves\n" +
+                "");
+        }
+    }
+}
